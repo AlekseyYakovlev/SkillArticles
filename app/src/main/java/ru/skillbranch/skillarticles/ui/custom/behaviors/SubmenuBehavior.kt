f282a2e5de167d20fac9e6c3e5c4b7dcb9abe7d1
Aleksey <aleksey@yakovlev.spb.ru>
2020-01-16 17:33:48 +0300

    Added custom behaviors for bottom bar and submenu

--- app/src/main/java/ru/skillbranch/skillarticles/ui/custom/behaviors/SubmenuBehavior.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/custom/behaviors/SubmenuBehavior.kt
@@ -1,4 +1,29 @@
 package ru.skillbranch.skillarticles.ui.custom.behaviors
 
-class SubmenuBehavior {
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.view.ViewCompat
+import androidx.core.view.marginEnd
+import kotlin.math.max
+import kotlin.math.min
+
+class SubmenuBehavior <V : View>(context: Context, attrs: AttributeSet) :
+    CoordinatorLayout.Behavior<V>(context, attrs) {
+
+    override fun onStartNestedScroll(
+        coordinatorLayout: CoordinatorLayout, child: V, directTargetChild: View, target: View, axes: Int, type: Int
+    ): Boolean {
+        return axes == ViewCompat.SCROLL_AXIS_VERTICAL
+    }
+
+    override fun onNestedPreScroll(
+        coordinatorLayout: CoordinatorLayout, child: V, target: View, dx: Int, dy: Int, consumed: IntArray, type: Int
+    ) {
+        super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed, type)
+        //child.translationY = max(0f, min(child.height.toFloat(), child.translationY + dy))
+        val childWidth = (child.marginEnd+child.width).toFloat()
+        child.translationX = max(0f, min(childWidth, child.translationX + dy*5))
+    }
 }
