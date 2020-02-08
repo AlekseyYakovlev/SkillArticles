5260a8de220293b35cdb36b0650ceca3e1139cfd
Aleksey <aleksey@yakovlev.spb.ru>
2020-02-08 22:49:30 +0300

    ViewModelDelegate fixed

--- app/src/main/java/ru/skillbranch/skillarticles/ui/base/BaseActivity.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/base/BaseActivity.kt
@@ -2,17 +2,20 @@ package ru.skillbranch.skillarticles.ui.base
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModel
 import ru.skillbranch.skillarticles.viewmodels.base.BaseViewModel
 import ru.skillbranch.skillarticles.viewmodels.base.IViewModelState
 import ru.skillbranch.skillarticles.viewmodels.base.Notify
+import ru.skillbranch.skillarticles.viewmodels.base.ViewModelDelegate
 
 abstract class BaseActivity<T : BaseViewModel<out IViewModelState>> : AppCompatActivity() {
-    protected abstract val binding:Binding
-    protected abstract val viewModel : T
-    protected abstract val layout:Int
+    protected abstract val binding: Binding
+    protected abstract val viewModel: T
+    protected abstract val layout: Int
 
     //set listeners, tuning views
     abstract fun setupViews()
+
     abstract fun renderNotification(notify: Notify)
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +23,8 @@ abstract class BaseActivity<T : BaseViewModel<out IViewModelState>> : AppCompatA
         setContentView(layout)
         setupViews()
         binding.onFinishInflate()
-        viewModel.observeState(this){binding.bind(it)}
-        viewModel.observeNotifications(this){renderNotification(it)}
+        viewModel.observeState(this) { binding.bind(it) }
+        viewModel.observeNotifications(this) { renderNotification(it) }
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
@@ -35,4 +38,7 @@ abstract class BaseActivity<T : BaseViewModel<out IViewModelState>> : AppCompatA
         viewModel.restoreState(savedInstanceState)
         binding.restoreUi(savedInstanceState)
     }
+
+    internal inline fun <reified T : ViewModel> provideViewModel(arg: Any?) =
+        ViewModelDelegate(T::class.java, arg)
 }
