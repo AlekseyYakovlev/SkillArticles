6a42a4f37e214ae23e10c4cca4b2f7784de48667
Aleksey <aleksey@yakovlev.spb.ru>
2020-02-19 23:20:07 +0300

    PrefDelegate fixes

--- app/src/main/java/ru/skillbranch/skillarticles/data/local/PrefManager.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/data/local/PrefManager.kt
@@ -2,13 +2,10 @@ package ru.skillbranch.skillarticles.data.local
 
 import android.content.Context
 import android.content.SharedPreferences
+import androidx.preference.PreferenceManager
 
 class PrefManager(context: Context) {
-    val preferences: SharedPreferences
-
-    init {
-        preferences = PrefManager(context).preferences
-    }
+    internal val preferences: SharedPreferences by lazy { PreferenceManager(context).sharedPreferences }
 
     fun clearAll() {
         preferences.edit().clear().apply()
