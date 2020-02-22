864f0114e5c0e5eeb9fbbbf69b42215f32d99998
Aleksey <aleksey@yakovlev.spb.ru>
2020-02-22 23:54:43 +0300

    tasks 1-3

--- app/src/main/java/ru/skillbranch/skillarticles/extensions/Context.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/extensions/Context.kt
@@ -1,9 +1,6 @@
 package ru.skillbranch.skillarticles.extensions
 
 import android.content.Context
-import android.net.ConnectivityManager
-import android.net.NetworkCapabilities
-import android.os.Build
 import android.util.TypedValue
 
 fun Context.dpToPx(dp: Int): Float {
@@ -22,6 +19,15 @@ fun Context.dpToIntPx(dp: Int): Int {
         this.resources.displayMetrics
     ).toInt()
 }
+
+
+fun Context.attrValue(res: Int):Int{
+    val tv = TypedValue()
+    return if (theme.resolveAttribute(res,tv,true)) tv.data else -1
+}
+
+
+
 
 //val Context.isNetworkAvailable: Boolean
 //    get() {
