f3c7ff565887c847b20ecfdb36fa9839c85b6b51
Aleksey <aleksey@yakovlev.spb.ru>
2020-02-22 23:55:14 +0300

    tasks 1-3

---
+++ app/src/main/java/ru/skillbranch/skillarticles/markdown/spans/InlineCodeSpan.kt
@@ -0,0 +1,91 @@
+package ru.skillbranch.skillarticles.markdown.spans
+
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.Typeface
+import android.text.style.ReplacementSpan
+import androidx.annotation.ColorInt
+import androidx.annotation.Px
+import androidx.annotation.VisibleForTesting
+
+class InlineCodeSpan(
+    @ColorInt
+    private val textColor: Int,
+    @ColorInt
+    private val bgColor: Int,
+    @Px
+    private val cornerRadius: Float,
+    @Px
+    private val padding: Float
+) : ReplacementSpan() {
+    @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+    var rect: RectF = RectF()
+    @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+    var measureWidth: Int = 0
+
+    override fun getSize(
+        paint: Paint,
+        text: CharSequence,
+        start: Int,
+        end: Int,
+        fm: Paint.FontMetricsInt?
+    ): Int {
+        paint.forText {
+            val measureText = paint.measureText(text.toString(), start, end)
+            measureWidth = (measureText + 2 * padding).toInt()
+        }
+        return measureWidth
+    }
+
+    override fun draw(
+        canvas: Canvas,
+        text: CharSequence,
+        start: Int,
+        end: Int,
+        x: Float,
+        top: Int,
+        y: Int,
+        bottom: Int,
+        paint: Paint
+    ) {
+        paint.forBackground {
+            rect.set(x, top.toFloat(), x + measureWidth, bottom.toFloat())
+            canvas.drawRoundRect(rect, cornerRadius, cornerRadius, paint)
+        }
+
+        paint.forText {
+            canvas.drawText(text, start, end, x + padding, y.toFloat(), paint)
+        }
+    }
+
+    private inline fun Paint.forText(block: () -> Unit) {
+        val oldSize = textSize
+        val oldStyle = typeface?.style ?: 0
+        val oldFont = typeface
+        val oldColor = color
+
+        color = textColor
+        typeface = Typeface.create(Typeface.MONOSPACE, oldStyle)
+        textSize *= 0.85f
+
+        block()
+
+        color = oldColor
+        typeface = oldFont
+        textSize = oldSize
+    }
+
+    private inline fun Paint.forBackground(block: () -> Unit) {
+        val oldColor = color
+        val oldStyle = style
+
+        color = bgColor
+        style = Paint.Style.FILL
+
+        block()
+
+        color = oldColor
+        style = oldStyle
+    }
+}
---
+++ app/src/main/java/ru/skillbranch/skillarticles/markdown/spans/UnorderedListSpan.kt
@@ -0,0 +1,53 @@
+package ru.skillbranch.skillarticles.markdown.spans
+
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.text.Layout
+import android.text.style.LeadingMarginSpan
+import androidx.annotation.ColorInt
+import androidx.annotation.Px
+
+
+class UnorderedListSpan(
+    @Px
+    private val gapWidth: Float,
+    @Px
+    private val bulletRadius: Float,
+    @ColorInt
+    private val bulletColor: Int
+) : LeadingMarginSpan {
+
+    override fun getLeadingMargin(first: Boolean): Int =
+        (4 * bulletRadius + gapWidth).toInt()
+
+    override fun drawLeadingMargin(
+        canvas: Canvas, paint: Paint, currentMarginLocation: Int, paragraphDirection: Int,
+        lineTop: Int, lineBaseline: Int, lineBottom: Int, text: CharSequence?, lineStart: Int,
+        lineEnd: Int, isFirstLine: Boolean, layout: Layout?
+    ) {
+        //draw bullet only for first line
+        if(isFirstLine){
+            paint.withCustomColor {
+                canvas.drawCircle(
+                    gapWidth+currentMarginLocation+bulletRadius,
+                    (lineTop+lineBottom)/2f,
+                    bulletRadius,
+                    paint
+                )
+            }
+        }
+    }
+
+    private inline fun Paint.withCustomColor(block: () -> Unit) {
+        val oldColor  = color
+        val oldStyle = style
+
+        color = bulletColor
+        style = Paint.Style.FILL
+
+        block()
+
+        color = oldColor
+        style = oldStyle
+    }
+}
