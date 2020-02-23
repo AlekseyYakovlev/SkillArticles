2342d25ee7dc0527e6e8a05358d86b759c11a8a8
Aleksey <aleksey@yakovlev.spb.ru>
2020-02-23 03:43:52 +0300

    bugs fix

--- app/src/main/java/ru/skillbranch/skillarticles/viewmodels/base/ViewModelDelegate.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/viewmodels/base/ViewModelDelegate.kt
@@ -3,6 +3,7 @@ package ru.skillbranch.skillarticles.viewmodels.base
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KProperty
 
@@ -14,12 +15,20 @@ class ViewModelDelegate<T : ViewModel>(
 
     private lateinit var viewModel: T
 
+//    override fun getValue(thisRef: FragmentActivity, property: KProperty<*>): T {
+//        if (!this::viewModel.isInitialized) {
+//            viewModel = when (arg) {
+//                null -> ViewModelProvider(thisRef).get(clazz)
+//                else -> ViewModelProvider(thisRef, ViewModelFactory(arg)).get(clazz)
+//            }
+//        }
+//        return viewModel
+//    }
+
     override fun getValue(thisRef: FragmentActivity, property: KProperty<*>): T {
-        if (!this::viewModel.isInitialized) {
-            viewModel = when (arg) {
-                null -> ViewModelProvider(thisRef).get(clazz)
-                else -> ViewModelProvider(thisRef, ViewModelFactory(arg)).get(clazz)
-            }
+        if (!this::viewModel.isInitialized) viewModel = when (arg) {
+            null -> ViewModelProviders.of(thisRef).get(clazz)
+            else -> ViewModelProviders.of(thisRef, ViewModelFactory(arg)).get(clazz)
         }
         return viewModel
     }
