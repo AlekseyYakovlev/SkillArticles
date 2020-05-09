e232b7fd03e8d7fdb24f07b7fc5c77415688c79b
Aleksey <aleksey@yakovlev.spb.ru>
2020-05-09 22:59:17 +0300

    build.gradle - libs versions extracted to standalone file

--- app/src/main/java/ru/skillbranch/skillarticles/viewmodels/articles/ArticlesViewModel.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/viewmodels/articles/ArticlesViewModel.kt
@@ -1,20 +1,41 @@
 package ru.skillbranch.skillarticles.viewmodels.articles
 
 import androidx.lifecycle.SavedStateHandle
-import ru.skillbranch.skillarticles.data.ArticleItemData
+import androidx.paging.DataSource
+import androidx.paging.LivePagedListBuilder
+import androidx.paging.PagedList
+import ru.skillbranch.skillarticles.data.models.ArticleItemData
 import ru.skillbranch.skillarticles.data.repositories.ArticlesRepository
 import ru.skillbranch.skillarticles.viewmodels.base.BaseViewModel
 import ru.skillbranch.skillarticles.viewmodels.base.IViewModelState
 
-class ArticlesViewModel(handle: SavedStateHandle) : BaseViewModel<ArticlesState>(handle, ArticlesState()) {
-    val repository = ArticlesRepository
+class ArticlesViewModel(handle: SavedStateHandle) :
+    BaseViewModel<ArticlesState>(handle, ArticlesState()) {
+    private val repository = ArticlesRepository
+    private val listConfig by lazy {
+        PagedList.Config.Builder()
+            .setEnablePlaceholders(false)
+            .setPageSize(10)
+            .setPrefetchDistance(30)
+            .setInitialLoadSizeHint(50)
+            .build()
+    }
 
     init {
-        subscribeOnDataSource(repository.loadArticles()){ articles, state ->
+        subscribeOnDataSource(repository.loadArticles()) { articles, state ->
             articles ?: return@subscribeOnDataSource null
             state.copy(articles = articles)
         }
     }
+
+    private fun buildPagedList(
+        dataFactory: DataSource.Factory<Int, ArticleItemData>
+    ) {
+        val builder = LivePagedListBuilder<Int, ArticleItemData>(
+            dataFactory,
+            listConfig
+        )
+    }
 }
 
-data class ArticlesState(val articles: List<ArticleItemData> = emptyList()): IViewModelState
+data class ArticlesState(val articles: List<ArticleItemData> = emptyList()) : IViewModelState
