8ddd5a10e741f20bf2b80247e3ddcf48c885f712
Aleksey <aleksey@yakovlev.spb.ru>
2020-08-21 02:22:52 +0300

    Network errors

---
+++ app/src/main/java/ru/skillbranch/skillarticles/data/remote/interceptors/ErrorStatusInterceptor.kt
@@ -0,0 +1,31 @@
+package ru.skillbranch.skillarticles.data.remote.interceptors
+
+import com.squareup.moshi.JsonEncodingException
+import okhttp3.Interceptor
+import okhttp3.Response
+import ru.skillbranch.skillarticles.data.JsonConverter.moshi
+import ru.skillbranch.skillarticles.data.remote.err.ApiError
+import ru.skillbranch.skillarticles.data.remote.err.ErrorBody
+
+class ErrorStatusInterceptor : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val response = chain.proceed(chain.request())
+
+        if (response.isSuccessful) return response
+
+        val errMessage = try {
+            moshi.adapter(ErrorBody::class.java).fromJson(response.body!!.string())?.message
+        } catch (e: JsonEncodingException) {
+            e.message
+        }
+
+        when (response.code) {
+            400 -> throw ApiError.BadRequest(errMessage)
+            401 -> throw ApiError.Unauthorized(errMessage)
+            403 -> throw ApiError.Forbidden(errMessage)
+            404 -> throw ApiError.NotFound(errMessage)
+            500 -> throw ApiError.InternalServerError(errMessage)
+            else -> throw ApiError.UnknownError(errMessage)
+        }
+    }
+}
