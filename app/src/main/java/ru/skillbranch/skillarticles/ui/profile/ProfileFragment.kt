010a947692dfe42d790885d590bc95fb5e1b110a
Aleksey <aleksey@yakovlev.spb.ru>
2020-08-21 02:21:21 +0300

    Profile fragment

--- app/src/main/java/ru/skillbranch/skillarticles/ui/profile/ProfileFragment.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/profile/ProfileFragment.kt
@@ -1,33 +1,109 @@
 package ru.skillbranch.skillarticles.ui.profile
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.graphics.drawable.Drawable
 import androidx.fragment.app.viewModels
-
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
+import kotlinx.android.synthetic.main.fragment_profile.*
 import ru.skillbranch.skillarticles.R
+import ru.skillbranch.skillarticles.extensions.dpToIntPx
+import ru.skillbranch.skillarticles.ui.base.BaseFragment
+import ru.skillbranch.skillarticles.ui.base.Binding
+import ru.skillbranch.skillarticles.ui.custom.ShimmerDrawable
+import ru.skillbranch.skillarticles.ui.delegates.RenderProp
+import ru.skillbranch.skillarticles.viewmodels.base.IViewModelState
+import ru.skillbranch.skillarticles.viewmodels.profile.ProfileState
 import ru.skillbranch.skillarticles.viewmodels.profile.ProfileViewModel
+import kotlin.properties.Delegates
 
-class ProfileFragment : Fragment() {
+class ProfileFragment : BaseFragment<ProfileViewModel>() {
 
-    companion object {
-        fun newInstance() = ProfileFragment()
+    override val viewModel: ProfileViewModel by viewModels()
+    override val layout: Int = R.layout.fragment_profile
+    override val binding: Binding by lazy { ProfileBinding() }
+
+
+    var cornerRadius by Delegates.notNull<Int>()
+    lateinit var avatarShimmer: ShimmerDrawable
+
+    override fun setupViews() {
+        val avatarSize = iv_avatar.maxWidth
+        cornerRadius = root.dpToIntPx(avatarSize / 2)
+
+        val baseColor = root.getColor(R.color.color_gray_light)
+        val highlightColor = requireContext().getColor(R.color.color_divider)
+
+        avatarShimmer = ShimmerDrawable.Builder()
+            .setBaseColor(baseColor)
+            .setHighlightColor(highlightColor)
+            .setShimmerWidth(avatarSize)
+            .addShape(ShimmerDrawable.Shape.Round(avatarSize))
+            .build()
+            .apply { start() }
+
+
     }
 
-    private val viewModel: ProfileViewModel by viewModels()
+    private fun updateAvatar(avatarUrl: String) {
+        Glide.with(root)
+            .load(avatarUrl)
+            .listener(object : RequestListener<Drawable> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    isFirstResource: Boolean
+                ): Boolean = false
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+                override fun onResourceReady(
+                    resource: Drawable?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    avatarShimmer.stop()
+                    return false
+                }
+
+            })
+            .placeholder(avatarShimmer)
+            .transform(CenterCrop(), RoundedCorners(cornerRadius))
+            .into(iv_avatar)
+
+
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        // TODO: Use the ViewModel
-    }
+    inner class ProfileBinding() : Binding() {
+        var avatar by RenderProp("") {
+            updateAvatar(it)
+        }
 
+        var name by RenderProp("") {
+            tv_name.text = it
+        }
+        var about by RenderProp("") {
+            tv_about.text = it
+        }
+        var rating by RenderProp(0) {
+            tv_rating.text = "Rating: $it"
+        }
+        var respect by RenderProp(0) {
+            tv_respect.text = "Respect: $it"
+        }
+
+        override fun bind(data: IViewModelState) {
+            data as ProfileState
+            data.avatar?.let { avatar = it }
+            data.name?.let { name = it }
+            data.about?.let { about = it }
+            data.rating.let { rating = it }
+            data.respect.let { respect = it }
+        }
+    }
 }
--- app/src/main/java/ru/skillbranch/skillarticles/viewmodels/profile/ProfileViewModel.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/viewmodels/profile/ProfileViewModel.kt
@@ -1,5 +1,36 @@
 package ru.skillbranch.skillarticles.viewmodels.profile
 
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.SavedStateHandle
+import ru.skillbranch.skillarticles.data.repositories.ProfileRepository
+import ru.skillbranch.skillarticles.viewmodels.base.BaseViewModel
+import ru.skillbranch.skillarticles.viewmodels.base.IViewModelState
 
-class ProfileViewModel : ViewModel()
+class ProfileViewModel(handle: SavedStateHandle) :
+    BaseViewModel<ProfileState>(handle, ProfileState()) {
+
+    val repository = ProfileRepository
+
+    init {
+        subscribeOnDataSource(repository.getProfile()) { profile, state ->
+            profile?.run {
+                state.copy(
+                    avatar = avatar,
+                    name = name,
+                    about = about,
+                    rating = rating,
+                    respect = respect
+
+                )
+            }
+        }
+    }
+
+}
+
+data class ProfileState(
+    val avatar: String? = null,
+    val name: String? = null,
+    val about: String? = null,
+    val rating: Int = 0,
+    val respect: Int = 0
+) : IViewModelState
