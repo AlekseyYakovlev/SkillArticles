b6c4f8e819768b3872295c98a8b50306814d1193
Aleksey <aleksey@yakovlev.spb.ru>
2020-08-21 02:26:20 +0300

    AppConfig.kt modified for testing

--- app/src/main/java/ru/skillbranch/skillarticles/AppConfig.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/AppConfig.kt
@@ -1,5 +1,6 @@
 package ru.skillbranch.skillarticles
 
 object AppConfig {
-    const val BASE_URL ="https://skill-articles.skill-branch.ru/api/v1/"
+    //    const val BASE_URL ="https://skill-articles.skill-branch.ru/api/v1/"
+    var BASE_URL = "https://skill-articles.skill-branch.ru/api/v1/"
 }
