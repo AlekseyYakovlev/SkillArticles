dbc7fde3c83973fa902d2294825f3c1e868895b2
Aleksey <31004501+AlekseyYakovlev@users.noreply.github.com>
2020-08-27 00:05:33 +0300

    TokenAuthenticator refactoring

--- app/src/main/java/ru/skillbranch/skillarticles/data/remote/interceptors/TokenAuthenticator.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/data/remote/interceptors/TokenAuthenticator.kt
@@ -7,32 +7,31 @@ import okhttp3.Route
 import ru.skillbranch.skillarticles.data.local.PrefManager
 import ru.skillbranch.skillarticles.data.remote.NetworkManager
 import ru.skillbranch.skillarticles.data.remote.req.RefreshReq
-import java.io.IOException
 
 class TokenAuthenticator : Authenticator {
     private val prefs = PrefManager
+    private val api by lazy { NetworkManager.api }
 
-    @Throws(IOException::class)
     override fun authenticate(route: Route?, response: Response): Request? {
         return if (prefs.accessToken.isNotEmpty() && response.code == 401) {
-//        return if (response.code == 401) {
-            val updatedToken = getNewToken()
-            response.request.newBuilder()
-                .header("Authorization", updatedToken)
-                .build()
+            getNewToken()?.let {
+                response.request.newBuilder()
+                    .header("Authorization", it)
+                    .build()
+            }
         } else null
     }
 
-    private fun getNewToken(): String {
-        val network = NetworkManager.api
+    private fun getNewToken(): String? {
         val oldRefreshToken = prefs.refreshToken
-        val response = network.refreshToken(RefreshReq(oldRefreshToken)).execute().body()!!
-        val newAccessToken = "Bearer ${response.accessToken}"
-//        Log.d("123456 New A token ", newAccessToken)
-//        Log.d("123456 New R token ", response.refreshToken)
-        prefs.accessToken = newAccessToken
-        prefs.refreshToken = response.refreshToken
-
-        return newAccessToken
+        val response = api.refreshToken(RefreshReq(oldRefreshToken)).execute()
+        return if (response.isSuccessful) {
+            response.body()?.let {
+                val newAccessToken = "Bearer ${it.accessToken}"
+                prefs.accessToken = newAccessToken
+                prefs.refreshToken = it.refreshToken
+                newAccessToken
+            }
+        } else null
     }
 }
